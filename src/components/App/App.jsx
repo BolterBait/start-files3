876954e7252a6bcf47dc0f139211fc2a97b88619
Darymee <danieledaniele3005@gmail.com>
2022-11-13 12:10:00 +0100

    added task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,18 @@
 import { Header } from 'components';
 import { CountrySearch, Home, Country } from 'pages';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
-  return <h2>App</h2>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route element={<Home />} index />
+          <Route path="/country" element={<CountrySearch />} />
+          <Route path="/country/:id" element={<Country />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,30 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
 
+import { useGetCountry } from 'hooks/useGetCountry';
+import { Link, Navigate, useLocation } from 'react-router-dom';
+
 export const Country = () => {
+  const location = useLocation();
+  const goBackLink = location?.state?.from ?? '/';
+
+  const { country, error, isLoading } = useGetCountry();
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <div
+          style={{
+            marginBottom: '60px',
+            color: '#F2F2F2',
+            letterSpacing: '0.06em',
+            borderColor: 'grey',
+          }}
+        >
+          <Link to={goBackLink}>Go Back</Link>
+        </div>
+        {isLoading && <Loader />}
+        {error && <Navigate to={'/'} replace />}
+        <CountryInfo country={country} />
       </Container>
     </Section>
   );
